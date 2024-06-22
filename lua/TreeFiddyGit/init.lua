1a4c532df94dbf109605477615b2da15d5802e6a
Bas Donker <23454780+DarkBones@users.noreply.github.com>
2024-06-22 17:59:04 +0200

    Remove bare repo check

--- lua/TreeFiddyGit/init.lua
+++ lua/TreeFiddyGit/init.lua
@@ -2,27 +2,6 @@ local Job = require("plenary.job")
 local telescope = require("telescope")
 local worktree_parser = require("TreeFiddyGit.parsers.worktrees_parser")
 
-local function is_bare_repo(callback)
-    Job:new({
-        command = "git",
-        args = { "rev-parse", "--is-bare-repository" },
-        on_exit = function(j, return_val)
-            if return_val == 0 then
-                local output = j:result()
-                if output[1] == "false" then
-                    print("Not a bare repository. Please use a bare repository.")
-                    callback(false)
-                else
-                    callback(true)
-                end
-            else
-                print("Error running git rev-parse --is-bare-repository")
-                callback(false)
-            end
-        end,
-    }):start()
-end
-
 local M = {}
 
 M.hello = function()
@@ -34,24 +13,19 @@ M.setup = function()
 end
 
 M.get_git_worktrees = function(callback)
-    is_bare_repo(function(is_bare)
-        if not is_bare then
-            return
-        end
-        Job:new({
-            command = "git",
-            args = { "worktree", "list" },
-            on_exit = function(j, return_val)
-                if return_val == 0 then
-                    local output = j:result()
-                    local parsed_output = worktree_parser.parse_worktrees(output)
-                    callback(parsed_output)
-                else
-                    print("Error running git worktree list")
-                end
-            end,
-        }):start()
-    end)
+    Job:new({
+        command = "git",
+        args = { "worktree", "list" },
+        on_exit = function(j, return_val)
+            if return_val == 0 then
+                local output = j:result()
+                local parsed_output = worktree_parser.parse_worktrees(output)
+                callback(parsed_output)
+            else
+                print("Error running git worktree list")
+            end
+        end,
+    }):start()
 end
 
 return M
