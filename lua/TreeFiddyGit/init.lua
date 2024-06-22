abad84ecb675329817de98faf77e9fbfcb8f6b83
Bas Donker <23454780+DarkBones@users.noreply.github.com>
2024-06-22 13:33:15 +0200

    Get worktrees list with plenary job

--- lua/TreeFiddyGit/init.lua
+++ lua/TreeFiddyGit/init.lua
@@ -1,3 +1,4 @@
+local Job = require("plenary.job")
 local telescope = require("telescope")
 
 local M = {}
@@ -10,8 +11,19 @@ M.setup = function()
     require("telescope").load_extension("tree_fiddy_git")
 end
 
-M.get_git_worktrees = function()
-    return { "main", "feature-a", "someotherbranch", "these-are-hard-coded-btw" }
+M.get_git_worktrees = function(callback)
+    Job:new({
+        command = "git",
+        args = { "worktree", "list" },
+        on_exit = function(j, return_val)
+            if return_val == 0 then
+                local output = j:result()
+                callback(output)
+            else
+                print("Error running git worktree list")
+            end
+        end,
+    }):start()
 end
 
 return M
--- lua/telescope/_extensions/tree_fiddy_git.lua
+++ lua/telescope/_extensions/tree_fiddy_git.lua
@@ -10,24 +10,28 @@ local git_worktrees = require("TreeFiddyGit").get_git_worktrees
 local tree_fiddy_git = function(opts)
     opts = opts or {}
 
-    pickers
-        .new(opts, {
-            prompt_title = "Select Git Worktree",
-            finder = finders.new_table({
-                results = git_worktrees(),
-            }),
-            sorter = sorters.get_generic_fuzzy_sorter(),
-            attach_mappings = function(prompt_bufnr, map)
-                actions.select_default:replace(function()
-                    actions.close(prompt_bufnr)
-                    local selection = action_state.get_selected_entry()
-                    local message = vim.inspect(selection)
-                    vim.api.nvim_echo({ { message } }, false, {})
-                end)
-                return true
-            end,
-        })
-        :find()
+    git_worktrees(function(worktrees)
+        vim.schedule(function()
+            pickers
+                .new(opts, {
+                    prompt_title = "Select Git Worktree",
+                    finder = finders.new_table({
+                        results = worktrees,
+                    }),
+                    sorter = sorters.get_generic_fuzzy_sorter(),
+                    attach_mappings = function(prompt_bufnr, map)
+                        actions.select_default:replace(function()
+                            actions.close(prompt_bufnr)
+                            local selection = action_state.get_selected_entry()
+                            local message = vim.inspect(selection)
+                            vim.api.nvim_echo({ { message } }, false, {})
+                        end)
+                        return true
+                    end,
+                })
+                :find()
+        end)
+    end)
 end
 
 return telescope.register_extension({
