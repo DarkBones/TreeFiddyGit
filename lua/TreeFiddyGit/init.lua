5fdfc1fdf8da9301055331821db3e4397c9b7f29
Bas Donker <23454780+DarkBones@users.noreply.github.com>
2024-06-22 16:25:46 +0200

    Cleanup

--- lua/TreeFiddyGit/init.lua
+++ lua/TreeFiddyGit/init.lua
@@ -11,25 +11,6 @@ M.setup = function()
     require("telescope").load_extension("tree_fiddy_git")
 end
 
--- local parse_worktrees = function(worktrees)
---     local parsed_worktrees = {}
---
---     for _, worktree in ipairs(worktrees) do
---         local path, hash, branch = worktree:match("(.-)%s+(%w+)%s+%[(.*)%]")
---         if path and hash and branch then
---             table.insert(parsed_worktrees, { path = path, hash = hash, branch = branch })
---         else
---             -- Handle the case where the worktree is bare and doesn't have a hash or branch
---             local path = worktree:match("(.-)%s+%((.*)%)")
---             if path then
---                 table.insert(parsed_worktrees, { path = path, hash = "", branch = "" })
---             end
---         end
---     end
---
---     return parsed_worktrees
--- end
-
 M.get_git_worktrees = function(callback)
     Job:new({
         command = "git",
@@ -37,7 +18,6 @@ M.get_git_worktrees = function(callback)
         on_exit = function(j, return_val)
             if return_val == 0 then
                 local output = j:result()
-                -- local parsed_output = parse_worktrees(output)
                 local parsed_output = require("TreeFiddyGit.parsers.worktrees_parser").parse_worktrees(output)
                 callback(parsed_output)
             else
