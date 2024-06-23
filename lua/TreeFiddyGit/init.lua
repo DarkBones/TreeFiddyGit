c7824ea9dcebed0896cf2bd69f350386c003328b
Bas Donker <23454780+DarkBones@users.noreply.github.com>
2024-06-23 10:37:32 +0200

    Non-working buffer changes

--- lua/TreeFiddyGit/init.lua
+++ lua/TreeFiddyGit/init.lua
@@ -36,10 +36,32 @@ M.get_git_worktrees = function(callback)
 end
 
 M.on_worktree_selected = function(name, path)
+    print("\n-------------------------\n")
     local git_root = utils.get_git_root_path()
     local full_path = git_root .. "/" .. utils.make_relative(path, ".")
 
+    -- Change the directory of all open buffers
+    local old_git_root = vim.fn.getcwd()
+    print("old_git_root: " .. old_git_root)
+
     vim.cmd(M.config.change_directory_cmd .. " " .. full_path)
+
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        local buf_path = vim.api.nvim_buf_get_name(bufnr)
+        print("buf_path: " .. buf_path)
+
+        local new_buf_path = buf_path:gsub(old_git_root, full_path)
+        print("new_buf_path: " .. new_buf_path)
+
+        -- check if the new path exists
+        if vim.fn.filereadable(new_buf_path) == 1 then
+            print("Changing buffer path to: " .. new_buf_path)
+            vim.api.nvim_buf_set_name(bufnr, new_buf_path)
+            vim.api.nvim_command("edit!")
+        else
+            print("File does not exist: " .. new_buf_path)
+        end
+    end
 end
 
 return M
