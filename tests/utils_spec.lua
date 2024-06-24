27d5650fc7995db403530e97fe0a002ea17975ba
Bas Donker <23454780+DarkBones@users.noreply.github.com>
2024-06-24 20:50:59 +0200

    Fix tests

--- tests/utils_spec.lua
+++ tests/utils_spec.lua
@@ -12,102 +12,134 @@ describe("utils", function()
         end)
 
         it("should return the git root path when inside a worktree", function()
-            utils._get_git_worktree_reference = function()
-                return "/home/user/gitrepo.git/worktrees/main"
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback("/home/user/gitrepo.git/worktrees/main")
+                end)
             end
 
-            local root_path = utils.get_git_root_path()
-            assert.are.equal("/home/user/gitrepo.git", root_path)
+            utils.get_git_root_path(function(root_path)
+                assert.are.equal("/home/user/gitrepo.git", root_path)
+            end)
         end)
 
         it("should handle multiple '.git' instances in the path", function()
-            utils._get_git_worktree_reference = function()
-                return "/home/user.git/gitrepo.git/worktrees/main"
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback("/home/user.git/gitrepo.git/worktrees/main")
+                end)
             end
 
-            local root_path = utils.get_git_root_path()
-            assert.are.equal("/home/user.git/gitrepo.git", root_path)
+            utils.get_git_root_path(function(root_path)
+                assert.are.equal("/home/user.git/gitrepo.git", root_path)
+            end)
         end)
 
         it("should handle a worktree having '.git' in the path", function()
-            utils._get_git_worktree_reference = function()
-                return "/home/user/gitrepo.git/worktrees/main.git"
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback("/home/user/gitrepo.git/worktrees/main.git")
+                end)
             end
 
-            local root_path = utils.get_git_root_path()
-            assert.are.equal("/home/user/gitrepo.git", root_path)
+            utils.get_git_root_path(function(root_path)
+                assert.are.equal("/home/user/gitrepo.git", root_path)
+            end)
         end)
 
         it("should handle multiple '.git/worktrees' in the path", function()
-            utils._get_git_worktree_reference = function()
-                return "/home/user/gitrepo.git/worktrees/main.git/worktrees/main"
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback("/home/user/gitrepo.git/worktrees/main.git/worktrees/main")
+                end)
             end
 
-            local root_path = utils.get_git_root_path()
-            assert.are.equal("/home/user/gitrepo.git/worktrees/main.git", root_path)
+            utils.get_git_root_path(function(root_path)
+                assert.are.equal("/home/user/gitrepo.git/worktrees/main.git", root_path)
+            end)
         end)
 
         it("should handle being in the bare repo", function()
-            utils._get_git_worktree_reference = function()
-                return "."
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback(".")
+                end)
             end
 
-            utils._get_pwd = function()
-                return "/home/user/gitrepo.git"
+            utils._get_pwd = function(callback)
+                vim.schedule(function()
+                    callback("/home/user/gitrepo.git")
+                end)
             end
 
-            local root_path = utils.get_git_root_path()
-            assert.are.equal("/home/user/gitrepo.git", root_path)
+            utils.get_git_root_path(function(root_path)
+                assert.are.equal("/home/user/gitrepo.git", root_path)
+            end)
         end)
 
         it("should fail when in a treeless repo", function()
-            utils._get_git_worktree_reference = function()
-                return ".git"
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback(".git")
+                end)
             end
 
-            assert.has_error(function()
-                utils.get_git_root_path()
+            utils.get_git_root_path(function(output)
+                assert.has_error(output)
             end)
         end)
 
         it("should fail when not in a supported repo", function()
-            utils._get_git_worktree_reference = function()
-                return nil
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback(nil)
+                end)
             end
 
-            assert.has_error(function()
-                utils.get_git_root_path()
+            utils.get_git_root_path(function(output)
+                assert.has_error(function()
+                    utils.get_git_root_path()
+                end)
             end)
         end)
 
         it("should handle when git root path is a subdirectory of the current directory", function()
-            utils._get_git_worktree_reference = function()
-                return "./subdirectory/.git"
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback("./subdirectory/.git")
+                end)
             end
 
-            local root_path = utils.get_git_root_path()
-            assert.are.equal("./subdirectory/.git", root_path)
+            utils.get_git_root_path(function(root_path)
+                assert.are.equal("./subdirectory/.git", root_path)
+            end)
         end)
 
         it("should handle when git root path is a relative path", function()
-            utils._get_git_worktree_reference = function()
-                return "./relative/path/.git"
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback("./relative/path/.git")
+                end)
             end
 
-            local root_path = utils.get_git_root_path()
-            assert.are.equal("./relative/path/.git", root_path)
+            utils.get_git_root_path(function(root_path)
+                assert.are.equal("./relative/path/.git", root_path)
+            end)
         end)
 
         it("should handle when worktree is named 'sometree.git'", function()
-            utils._get_git_worktree_reference = function()
-                return "/home/user/gitrepo.git/worktrees/sometree.git"
+            utils._get_git_worktree_reference = function(callback)
+                vim.schedule(function()
+                    callback("/home/user/gitrepo.git/worktrees/sometree.git")
+                end)
             end
 
-            local root_path = utils.get_git_root_path()
-            assert.are.equal("/home/user/gitrepo.git", root_path)
+            utils.get_git_root_path(function(root_path)
+                assert.are.equal("/home/user/gitrepo.git", root_path)
+            end)
         end)
     end)
-
+    --
     describe("update_file_path_to_new_worktree()", function()
         it("should return the new filepath", function()
             local old_git_path = "/home/user/gitrepo.git/main/nested/tree"
